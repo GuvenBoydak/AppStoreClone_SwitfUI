be7f843fcd93ba40723b4981bff73cba123ba98b
Güven Boydak <80548551+GuvenBoydak@users.noreply.github.com>
2024-01-17 11:55:15 +0300

    Initial Commit

---
+++ AppStoreClone/AppStoreCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppStoreCloneApp.swift
+//  AppStoreClone
+//
+//  Created by Güven Boydak on 17.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct AppStoreCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
