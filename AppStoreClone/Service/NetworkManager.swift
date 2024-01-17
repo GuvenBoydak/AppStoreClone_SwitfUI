5fa1ab8d02dae133edfaff6c7783770278564ee7
Güven Boydak <80548551+GuvenBoydak@users.noreply.github.com>
2024-01-17 16:25:47 +0300

    add networkManager

---
+++ AppStoreClone/Service/NetworkManager.swift
@@ -0,0 +1,49 @@
+//
+//  NetworkManager.swift
+//  AppStoreClone
+//
+//  Created by Güven Boydak on 17.01.2024.
+//
+
+import Foundation
+
+
+enum HTTPMethod: String {
+    case get = "GET"
+}
+
+enum ErrorMessage: Error {
+    case URLError
+    case DataParseErrror
+}
+
+enum Path: String {
+    case base_url = "https://itunes.apple.com/"
+    case search = "search?term="
+}
+
+
+protocol NetworkProtocol {
+    func fetchREquest<T: Codable>(path: String,method: HTTPMethod) async throws -> T
+}
+
+final class NetworkManager: NetworkProtocol {
+
+    func fetchREquest<T: Codable>(path: String, method: HTTPMethod) async throws -> T  {
+        guard let url = URL(string: "\(Path.base_url.rawValue)\(path)") else {
+            throw ErrorMessage.URLError
+        }
+        var urlRequest = URLRequest(url: url)
+        urlRequest.httpMethod = method.rawValue
+        
+        let urlSession = URLSession.shared
+        do {
+            let (data,_) = try await urlSession.data(for: urlRequest)
+            let result = try JSONDecoder().decode(T.self, from: data)
+            return result
+        } catch  {
+            print(error)
+            throw ErrorMessage.DataParseErrror
+        }
+    }
+}
